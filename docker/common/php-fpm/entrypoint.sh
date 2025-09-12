1e397c75769284076a398263e22d2bb72f0799e2
Asdin khmaich <asdin986@gmail.com>
2025-09-12 15:06:40 +0200

    Docker changes

--- docker/common/php-fpm/entrypoint.sh
+++ docker/common/php-fpm/entrypoint.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-set -e
-
 # Ajustar permisos
 chown -R www-data:www-data /var/www
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
-# Ejecutar Artisan caches solo si existe .env
-if [ -f /var/www/.env ]; then
-    echo "Caching Laravel config, routes, views..."
-    php artisan config:cache
-    php artisan route:cache
-    php artisan view:cache
+# Ejecutar cache solo si artisan existe
+if [ -f /var/www/artisan ]; then
+    php /var/www/artisan config:cache
+    php /var/www/artisan route:cache
+    php /var/www/artisan view:cache
 fi
 
-# Ejecutar PHP-FPM
-exec php-fpm
+# Mantener el contenedor corriendo
+php-fpm
